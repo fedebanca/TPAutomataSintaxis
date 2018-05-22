c3f5ed872ada21c7939da3464f9d5cf1b8c4e3ad
Maximiliano Nicoletti <mnicoletti.gf@gmail.com>
2018-05-22 19:08:13 -0300

    Agregado salida de archivo con output final

--- Automata.c
+++ Automata.c
@@ -11,16 +11,25 @@ int darColumna(char caracter);
 void main() {
     // Variables y Constantes
     char fnameInsert[] = "cadenas.txt";
+    char fnameOutput[] = "cadenaSalida.txt";
     FILE *punteroFile;
+    FILE *fileSalida;
     char *lineas = NULL;
     size_t len = 0;
     ssize_t lectura;
     char *palabras = malloc(sizeof(char)*20);
+    int estadoFinal;
 
     punteroFile = fopen(fnameInsert, "r"); // Apertura del archivo de texto, modo lectura unicamente.
     if(punteroFile == NULL) {
         exit(EXIT_FAILURE);
     }
+
+    fileSalida = fopen(fnameOutput, "w");
+    if(fileSalida == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
 
     while((lectura = getline(&lineas,&len,punteroFile) != -1)){ // Leo el archivo linea por linea.
         // Para hacer splits de un str, usamos la funcion strtok de la libreria string.h
@@ -28,12 +37,31 @@ void main() {
         palabras = strtok(lineas, " ");
         
         while(palabras != NULL){
-            automata(palabras);
+            estadoFinal = automata(palabras);
+            switch(estadoFinal) {
+                case 1:
+                    fprintf(fileSalida, "La palabra %s es un decimal.\n", palabras);
+                    break;
+                case 5:
+                    fprintf(fileSalida, "La palabra %s es un octal.\n", palabras);
+                    break;
+                case 2:
+                case 3:
+                    fprintf(fileSalida, "La palabra %s es un hexadecimal.\n", palabras);
+                    break;
+                case 4:
+                case 6:
+                    fprintf(fileSalida, "La palabra %s no es un entero valido.\n", palabras);
+                    break;
+                default:
+                    fprintf(fileSalida, "No se finalizo en un estado valido.");
+            }
             palabras = (NULL, " ");
         }
         
     }
     
+    fclose(fileSalida);
     fclose(punteroFile); // Cierre del archivo de texto.
     if(lineas) {
         free(lineas);
@@ -49,7 +77,7 @@ int automata(char *palabra) {
 	    estado = matrizDelAutomata[estado][columna];
 		posicionEnPalabra ++;
 	} while(estado != 6 || posicionEnPalabra < 20 || palabra[posicionEnPalabra] == '\n' );
-	return (estado);
+	return estado;
 }
 
 int darColumna(char caracter) {
